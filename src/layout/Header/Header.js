91c54304bd9bcc6894baa73ceee45d29f25dd2f5
Phong Ngo <ngophong010@outlook.com>
2025-08-17 20:06:18 +0700

    refactor: refactor Header for fe app

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -23,54 +23,61 @@ import { handleChangePage } from "../../redux-toolkit/paginationSlice";
 import { USER_MENU } from "../../utils/menu";
 import TippyCart from "../../components/TippyCart/TippyCart";
 import { convertSlugUrl } from "@/utils/commonUtils";
+import ProductTypesMenu from "@/components/ProductTypesMenu/ProductTypesMenu";
+import { useCallback } from "react";
 
-const ProductTypesMenu = () => {
-  const [productTypes, setProductTypes] = useState([]);
-  const dispatch = useDispatch();
-  const router = useRouter();
-  let getAllProductType = async () => {
-    try {
-      let res = await handleGetAllProductTypeService();
-      if (res && res.errCode === 0) {
-        setProductTypes(res?.data);
-      }
-    } catch (err) {
-      toast.error(err?.response?.data?.message);
-    }
-  };
-  useEffect(() => {
-    getAllProductType();
-  }, []);
+const USER_MENU_DELAY = [0, 300];
+const PRODUCT_MENU_DELAY = [0, 500];
+const CART_TIPPY_OFFSET = [-26, 5];
 
-  const handleChangProductType = (productTypeId, productTypeName) => {
-    dispatch(handleChangePage(1));
-    router.push(
-      `/${convertSlugUrl(productTypeName)}-${productTypeId.toLowerCase()}`
-    );
-    // router.push(`/product/${productTypeId}`);
-  };
+// const ProductTypesMenu = () => {
+//   const [productTypes, setProductTypes] = useState([]);
+//   const dispatch = useDispatch();
+//   const router = useRouter();
+//   let getAllProductType = async () => {
+//     try {
+//       let res = await handleGetAllProductTypeService();
+//       if (res && res.errCode === 0) {
+//         setProductTypes(res?.data);
+//       }
+//     } catch (err) {
+//       toast.error(err?.response?.data?.message);
+//     }
+//   };
+//   useEffect(() => {
+//     getAllProductType();
+//   }, []);
 
-  return (
-    <>
-      {productTypes.map((item, index) => {
-        return (
-          <div
-            key={index}
-            className="product-type-wrapper"
-            onClick={() =>
-              handleChangProductType(item.productTypeId, item.productTypeName)
-            }
-          >
-            <h1>{item.productTypeName}</h1>
-          </div>
-        );
-      })}
-    </>
-  );
-};
+//   const handleChangeProductType = (productTypeId, productTypeName) => {
+//     dispatch(handleChangePage(1));
+//     router.push(
+//       `/${convertSlugUrl(productTypeName)}-${productTypeId.toLowerCase()}`
+//     );
+//     // router.push(`/product/${productTypeId}`);
+//   };
+
+//   return (
+//     <>
+//       {productTypes.map((item, index) => {
+//         return (
+//           <div
+//             key={index}
+//             className="product-type-wrapper"
+//             onClick={() =>
+//               handleChangeProductType(item.productTypeId, item.productTypeName)
+//             }
+//           >
+//             <h1>{item.productTypeName}</h1>
+//           </div>
+//         );
+//       })}
+//     </>
+//   );
+// };
 
 function Header() {
   const [isTippyOn, setIsTippyOn] = useState(false);
+  const [productTypes, setProductTypes] = useState([]);
 
   const dispatch = useDispatch();
   const router = useRouter();
@@ -80,9 +87,28 @@ function Header() {
   const roleId = useSelector((state) => state.user.userInfo?.roleData?.roleId);
   const productCountInCart = useSelector((state) => state.cart?.totalProduct);
 
-  const handleLogOut = () => {
-    dispatch(logOut());
-  };
+  useEffect(() => {
+    const fetchProductTypes = async () => {
+      try {
+        const res = await handleGetAllProductTypeService();
+        if (res && res.errCode === 0) {
+          setProductTypes(res.data || []);
+        }
+      } catch (err) {
+        toast.error(err?.response?.data?.message || "Failed to load product types");
+      }
+    };
+    fetchProductTypes();
+  }, []);
+
+  // const handleLogOut = () => {
+  //   dispatch(logOut());
+  // };
+  const handleLogOut = useCallback(() => dispatch(logOut()), [dispatch]);
+
+  const handleProductTypeClick = useCallback(() => {
+    dispatch(handleChangePage(1));
+  }, [dispatch]);
 
   const handleClickCart = () => {
     router.push("/user/cart");
@@ -100,7 +126,7 @@ function Header() {
   return (
     <div className="header-container">
       <Link href="/">
-        <Image src="/images/logo1.png" alt="logo" height={50} width={100} />
+        <Image src="/images/color-logo.png" alt="logo" height={50} width={100} />
       </Link>
 
       <Tippy
@@ -112,11 +138,15 @@ function Header() {
         }}
         placement="bottom"
         interactive
-        delay={[0, 500]}
-        offset={[0, 3]}
+        delay={PRODUCT_MENU_DELAY}
+        // offset={[0, 3]}
         render={(attrs) => (
           <div className="drop-down-menu" tabIndex="-1" {...attrs}>
-            <ProductTypesMenu />
+            {/* <ProductTypesMenu /> */}
+            <ProductTypesMenu
+              productTypes={productTypes}
+              onProductTypeClick={handleProductTypeClick}
+            />
           </div>
         )}
       >
@@ -131,13 +161,13 @@ function Header() {
         </div>
       </Tippy>
 
-      <div onClick={handleClickSaleOff} className="btn-sale-off">
+      <Link href="/sale-off" className="btn-sale-off" onClick={() => dispatch(handleChangePage(1))}>
         SALE OFF
-      </div>
+      </Link>
 
-      <div onClick={handleClickFeed} className="btn-sale-off">
+      <Link href="/feed" className="btn-feed" onClick={{handleClickFeed}}>
         TIN TỨC
-      </div>
+      </Link>
 
       <Search></Search>
 
@@ -147,7 +177,7 @@ function Header() {
             placement="bottom-end"
             interactive
             delay={[0, 100]}
-            offset={[-26, 5]}
+            offset={CART_TIPPY_OFFSET}
             render={(attrs) => (
               <div
                 className="drop-down-menu"
@@ -159,12 +189,18 @@ function Header() {
               </div>
             )}
           >
-            <div className="header-cart" onClick={handleClickCart}>
+            {/* <div className="header-cart" onClick={handleClickCart}>
               <ShoppingCartOutlinedIcon className="header-cart-icon"></ShoppingCartOutlinedIcon>
               <span className="header-cart-notification">
                 {productCountInCart}
               </span>
-            </div>
+            </div> */}
+            <Link href="/user/cart" className="header-cart" onClick={handleClickCart}>
+              <ShoppingCartOutlinedIcon className="header-cart-icon"></ShoppingCartOutlinedIcon>
+              <span className="header-cart-notification">
+                {productCountInCart}
+              </span>
+            </Link>
           </Tippy>
         ) : null}
 
@@ -173,7 +209,7 @@ function Header() {
             <Tippy
               interactive
               placement="bottom-end"
-              delay={[0, 300]}
+              delay={USER_MENU_DELAY}
               render={(attrs) => (
                 <UserMenu
                   attrs={attrs}
@@ -186,7 +222,7 @@ function Header() {
               <button className="header-action-btn">
                 <Image
                   src={avatarUser ? avatarUser : "/images/default-avatar.png"}
-                  alt="avt"
+                  alt="User avatar"
                   height={50}
                   width={50}
                   className="action-avatar"
