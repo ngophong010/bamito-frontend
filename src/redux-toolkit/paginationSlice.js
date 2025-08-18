c9d7397d86872a92f81dc93f7a18cae5c9cd3f38
Phong Ngo <ngophong010@outlook.com>
2025-08-18 16:09:56 +0700

    refactor: refactor redux-toolkit/paginationSlice

--- src/redux-toolkit/paginationSlice.js
+++ src/redux-toolkit/paginationSlice.js
@@ -1,19 +1,57 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  page: 1,
+  paginators: {},
 };
 
 export const paginationSlice = createSlice({
   name: "pagination",
   initialState,
   reducers: {
-    handleChangePage: (state, action) => {
-      state.page = action.payload;
+    setPaginationData: (state, action) => {
+      const { paginatorId, page, limit, totalItems } = action.payload;
+      if (!state.paginators[paginatorId]) {
+        state.paginators[paginatorId] = {};
+      }
+      state.paginators[paginatorId].page = page;
+      state.paginators[paginatorId].limit = limit;
+      state.paginators[paginatorId].totalItems = totalItems;
     },
-  },
+    setPage: (state, action) => {
+      const { paginatorId, page } = action.payload;
+      if (state.paginators[paginatorId]) {
+        state.paginators[paginatorId].page = page;
+      }
+    },
+
+    nextPage: (state, action) => {
+      const { paginatorId } = action.payload;
+      const paginator = state.paginators[paginatorId];
+      if (paginator) {
+        const totalPages = Math.ceil(
+          paginator.totalItems / paginator.limit
+        );
+        if (paginator.page < totalPages) {
+          paginator.page += 1;
+        }
+      }
+    },
+    prevPage: (state, action) => {
+      const { paginatorId } = action.payload;
+      const paginator = state.paginators[paginatorId];
+      if (paginator && paginator.page > 1) {
+        paginator.page -= 1;
+      }
+    },
+    resetPaginatior: (state, action) => {
+      const { paginatorId } = action.payload;
+      if (state.paginators[paginatorId]) {
+        state.paginators[paginatorId].page= 1;
+      } 
+    }
+    },
 });
 
-export const { handleChangePage } = paginationSlice.actions;
+export const { setPaginationData, setPage, nextPage, prevPage, resetPaginator } = paginationSlice.actions;
 
 export default paginationSlice.reducer;
