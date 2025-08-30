b152678e0216e683fb313fad685f77568d80d54d
Phong Ngo Lam <ngophong010@gmail.com>
2025-08-30 12:46:35 +0700

    feat: update page

--- src/app/page.js
+++ src/app/page.js
@@ -2,76 +2,88 @@ import React from "react";
 import Banner from "@/components/Banner/Banner";
 import Introduce from "@/components/Introduce/Introduce";
 import Image from "next/image";
-import Grid from "@mui/material/Grid";
 import UserLayout from "@/layout/userLayout/UserLayout";
 import "./page.scss";
 
-function Home() {
-  const ld = {
+export async function generateMetadata() {
+  const jsonLd = {
     "@context": "https://schema.org",
-    "@type": "Home",
-    name: "Shop Badminton",
+    "@type": "Store",
+    name: "BAMITO Badminton Shop",
+    url: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
     image: "https://example.com/photos/yonex-racket.jpg",
-    description: "Trang Chủ - Cửa Hàng Đồ Cầu Lông Chính Hãng",
+    description: "Cửa hàng cầu lông chính hãng. Chúng tôi cung cấp vợt, giày, quần áo và phụ kiện chất lượng cao.",
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "123 Đường ABC",
+      addressLocality: "Thành phố Hồ Chí Minh",
+      postalCode: "700000",
+      addressCountry: "VN"
+    },
+    telephone: "+84123456789"
   };
+
+  return {
+    title: "Trang Chủ - Cửa Hàng Đồ Cầu Lông Chính Hãng",
+    description: "Chào mừng đến với cửa hàng đồ cầu lông chính hãng BMT. Chúng tôi cung cấp vợt, giày, quần áo và phụ kiện chất lượng cao với giá tốt nhất.",
+    alternates: {
+      canonical: "/", // The canonical URL for the homepage is the root
+    },
+    other: {
+      "application/ld+json": JSON.stringify(jsonLd),
+    }
+  };
+}
+
+function Home() {
   return (
     <UserLayout>
-      <script type="application/ld+json">{JSON.stringify(ld)}</script>
       <div className="home-container">
         <Banner></Banner>
         <div className="home-content">
           <Introduce></Introduce>
           <div className="home-benefit">
-            <Grid container spacing={12}>
-              <Grid item xs={4}>
-                <div className="home-benefit-wrap-item">
-                  <div className="home-benefit-item">
-                    <Image
-                      src="/images/policy_image_2 1.png"
-                      width={60}
-                      height={60}
-                      alt="badminton"
-                      className="home-benefit-item-img"
-                    ></Image>
-                    <div className="home-benefit-item-text">
-                      Vận chuyển toàn quốc, thanh toán khi nhận hàng
-                    </div>
-                  </div>
+            <div className="benefit-grid-container">
+              {/* Item 1 */}
+              <div className="benefit-item">
+                <Image
+                  src="/images/policy_image_2 1.png"
+                  width={60}
+                  height={60}
+                  alt="Vận chuyển toàn quốc"
+                  className="benefit-item-img"
+                />
+                <div className="benefit-item-text">
+                  Vận chuyển toàn quốc, thanh toán khi nhận hàng
                 </div>
-              </Grid>
-              <Grid item xs={4}>
-                <div className="home-benefit-wrap-item">
-                  <div className="home-benefit-item">
-                    <Image
-                      src="/images/thanh_toan 1.png"
-                      height={60}
-                      width={60}
-                      alt="badminton"
-                      className="home-benefit-item-img"
-                    ></Image>
-                    <div className="home-benefit-item-text">
-                      Tiến hành thanh toán với nhiều phương thức
-                    </div>
-                  </div>
+              </div>
+              {/* Item 2 */}
+              <div className="benefit-item">
+                <Image
+                  src="/images/thanh_toan 1.png"
+                  height={60}
+                  width={60}
+                  alt="Thanh toán đa dạng"
+                  className="benefit-item-img"
+                />
+                <div className="benefit-item-text">
+                  Tiến hành thanh toán với nhiều phương thức
                 </div>
-              </Grid>
-              <Grid item xs={4}>
-                <div className="home-benefit-wrap-item">
-                  <div className="home-benefit-item">
-                    <Image
-                      src="/images/policy_image_1 1.png"
-                      alt="badminton"
-                      height={60}
-                      width={60}
-                      className="home-benefit-item-img"
-                    ></Image>
-                    <div className="home-benefit-item-text">
-                      Sản phẩm đảm bảo chất lượng
-                    </div>
-                  </div>
+              </div>
+              {/* Item 3 */}
+              <div className="benefit-item">
+                <Image
+                  src="/images/policy_image_1 1.png"
+                  alt="Sản phẩm chất lượng"
+                  height={60}
+                  width={60}
+                  className="benefit-item-img"
+                />
+                <div className="benefit-item-text">
+                  Sản phẩm đảm bảo chất lượng
                 </div>
-              </Grid>
-            </Grid>
+              </div>
+            </div>
           </div>
         </div>
       </div>
