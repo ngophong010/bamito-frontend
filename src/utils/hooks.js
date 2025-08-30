21a0b6a9db1b6be631df1a2a11564a88e913b4d8
Phong Ngo Lam <ngophong010@gmail.com>
2025-08-30 12:45:14 +0700

    feat: update hooks of utils

---
+++ src/utils/hooks.js
@@ -0,0 +1,20 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debounceValue;
+};
+
+export {useDebounce}
